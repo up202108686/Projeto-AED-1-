242822990da82f1b1aea5dc940d6f8d88b488746
emts2002 <emts2002@gmail.com>
2022-11-02 15:56:25 +0000

    2nd commit

---
+++ Aula.h
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Aula {
+public:
+    Aula(uc, weekday, start_hour, duration, type, turma) {
+        this.uc = uc;
+        this.weekday = weekday;
+        this.start_hour = start_hour;
+        this.duration = duration;
+        this.type = type;
+        this.turma = turma;
+    }
+private:
+    UC uc;
+    string weekday;
+    float start_hour;
+    float duration;
+    string type;
+    Class turma;
+}
---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include "UC.h"
+#include "Class.h"
+#include "Student.h"
+#include "Horario.h"
+#include "Aula.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream infile("data/classes.cvs");
+    string line;
+    vector<UC> UCs;
+    vector<Student> students;
+
+    while (getline(infile, line, ',')) {
+        istringstream iss(line);
+        string turma, cadeira, dia, tipo;
+        float hora, duracao;
+        line >> turma >> cadeira >> dia >> hora >> duracao >> tipo;
+        for (UC uc : UCs) {
+            if (uc.getCode() == cadeira) {
+                for (Class cl : uc.getClasses()) {
+                    if (cl.getCode() == turma) {
+                        Aula a = new Aula(uc, dia, hora, duracao, tipo, cl);
+                        cl.addAula(a);
+                        break;
+                    }
+                    Class c = new Class(turma, uc);
+                    Aula a = new Aula(uc, dia, hora, duracao, tipo, cl);
+                    c.addAula(a);
+                    uc.addClass(c);
+                }
+                break;
+            }
+            UC u = new UC(cadeira);
+            Class c = new Class(turma, uc);
+            Aula a = new Aula(uc, dia, hora, duracao, tipo, cl);
+            c.addAula(a);
+            u.addClass(c);
+            UCs.push_back(u);
+        }
+    }
+    /*
+    for (UC uc : UCs) {
+        for (Class c : uc.getClasses()) {
+            cout << c.getCode();
+        }
+    }*/
+
+    return;
+}
